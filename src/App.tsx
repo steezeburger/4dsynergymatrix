678024a960b354314f94b235a7725837bb29c0c6
Jesse Snyder <jessetsnyder@gmail.com>
2023-04-20 22:06:53 -0600

    4d synergy matrix complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Canvas } from "@react-three/fiber";
 import './App.css';
+import Cylinder3d from "./components/Cylinder3d";
 
 function App() {
+  const numRows = 53;
+  const numCols = 53;
+  const spacing = 2;
+
+  const cubes = [];
+  for (let i = 0; i < numRows; i++) {
+    for (let j = 0; j < numCols; j++) {
+      cubes.push(
+        <Cylinder3d
+          key={`${i}-${j}`}
+          wireframe={false}
+          position={[
+            (i - numRows / 2) * spacing,
+            (j - numCols / 2) * spacing,
+            0
+          ]}
+        />
+      );
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas>
+        <pointLight position={[10, 10, 10]} />
+        {cubes}
+      </Canvas>
     </div>
   );
 }
---
+++ src/components/Cylinder3d.tsx
@@ -0,0 +1,71 @@
+import React, { useRef, useState, useMemo } from "react";
+import { useFrame, MeshProps } from "@react-three/fiber";
+import { Mesh, EdgesGeometry, LineBasicMaterial, LineSegments, ShaderMaterial, Group } from "three";
+
+type Cylinder3dProps = {
+  wireframe?: boolean;
+  position?: [number, number, number];
+} & MeshProps;
+
+const Cylinder3d: React.FC<Cylinder3dProps> = (props) => {
+  const groupRef = useRef<Group>(null);
+  const meshRef = useRef<Mesh>(null);
+  const [hovered, setHovered] = useState<boolean>(false);
+  const [clicked, setClicked] = useState<boolean>(false);
+
+  useFrame((state, delta) => {
+    if (groupRef.current) {
+      groupRef.current.rotation.x += 0.01;
+      groupRef.current.rotation.y += 0.01;
+      groupRef.current.rotation.z += 0.01;
+    }
+  });
+
+  const outline = useMemo(() => {
+    if (meshRef.current) {
+      const edgesGeometry = new EdgesGeometry(meshRef.current.geometry);
+      const lineMaterial = new LineBasicMaterial({ color: "black" });
+      return new LineSegments(edgesGeometry, lineMaterial);
+    }
+  }, [meshRef.current]);
+
+  const gridShaderMaterial = new ShaderMaterial({
+    vertexShader: `
+      varying vec3 vPosition;
+      void main() {
+        vPosition = position;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+      }
+    `,
+    fragmentShader: `
+      varying vec3 vPosition;
+      void main() {
+        vec3 color = vec3(0.0, 1.0, 0.0);
+        float lineWidth = 0.005;
+        float gridSize = 10.0;
+        vec3 modPosition = mod(vPosition * gridSize + 0.5, 1.0);
+        float mask = step(lineWidth, modPosition.x) * step(lineWidth, modPosition.y) * step(lineWidth, modPosition.z);
+        gl_FragColor = mix(vec4(color, 1.0), vec4(0.0, 0.0, 0.0, 1.0), mask);
+      }
+    `,
+  });
+
+  return (
+    <group ref={groupRef}>
+      <mesh
+        {...props}
+        ref={meshRef}
+        scale={clicked ? 1.5 : 1}
+        onClick={(event) => setClicked(!clicked)}
+        onPointerOver={(event) => setHovered(true)}
+        onPointerOut={(event) => setHovered(false)}
+      >
+        <boxGeometry args={[1, 1, 1]} />
+        <primitive object={gridShaderMaterial} />
+      </mesh>
+      {outline && <primitive object={outline} />}
+    </group>
+  );
+};
+
+export default Cylinder3d;
